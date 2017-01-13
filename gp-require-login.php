0ae3ee1f05443f62af0c66edd012e7adcc59cedd
Greg Ross <greg@toolstack.com>
2017-01-13 08:33:12 -0500

    Update code for redirect URL.
    Two issues resolved, $_SERVER['REQUEST_SCHEME'] is not available on all web servers (thanks @TiS) and previous code did not handle non-standard ports.

--- gp-require-login.php
+++ gp-require-login.php
@@ -1,14 +1,14 @@
 <?php
 /*
-Plugin Name: GP Require Login
-Plugin URI: http://glot-o-matic.com/gp-require-login
-Description: Redirect non-logged in users to the login page.
-Version: 0.5
-Author: GregRoss
-Author URI: http://toolstack.com
-Tags: glotpress, glotpress plugin, translate
-License: GPLv2 or later
-*/
+ * Plugin Name: GP Require Login
+ * Plugin URI: http://glot-o-matic.com/gp-require-login
+ * Description: Redirect non-logged in users to the login page.
+ * Version: 0.5
+ * Author: GregRoss
+ * Author URI: http://toolstack.com
+ * Tags: glotpress, glotpress plugin, translate
+ * License: GPLv2 or later
+ */
 
 class GP_Require_Login {
 	public $id = 'require-login';
@@ -20,11 +20,43 @@ class GP_Require_Login {
 	
 	public function gp_router_http_methods( $methods ) {
 		if( ! is_user_logged_in() ) {
-			wp_redirect( wp_login_url( $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ) );
+			wp_redirect( wp_login_url( $this->get_current_page_url() ) );
 			exit;
 		}
 		
 		return $methods;
+	}
+	
+	private function get_current_page_url() {
+		$page_url = 'http';
+		
+		// Ask WordPress if we're running over SSL.
+		$ssl = is_ssl();
+		
+		// Double check if we're running behind a broken load balancer.
+		if( false === $ssl ) {
+			if( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && 'https' === $_SERVER['HTTP_X_FORWARDED_PROTO'] ) {
+				$ssl = true;
+			}
+		}
+		
+		// Add the 's' to the url if required.
+		if( $ssl ) {
+			$page_url .= 's';
+		}
+		
+		// Add the server name to the url.
+		$page_url .= '://' . $_SERVER['SERVER_NAME'];
+		
+		// Check to see if we're running over a non-default port number, if so add it to the url.
+		if( ( ! $ssl && '80' !== $_SERVER['SERVER_PORT'] ) || ( $ssl && '443' !== $_SERVER['SERVER_PORT'] ) ) {
+			$page_url .= ':' . $_SERVER['SERVER_PORT'];
+		}
+
+		// And finally add the URI to the URL.
+		$page_url .= $_SERVER['REQUEST_URI'];
+		
+		return $page_url;
 	}
 	
 }
